68a161b267139dce3500e538d12f4f5556301ab5
smythp <patricksmyth01@gmail.com>
2016-11-14 13:48:29 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+## CracklePop in Common Lisp
+
+CracklePop written in Common Lisp. Note that there is no range function in CL, so that has also been implemented.
+
+If you have clisp installed, run:
+
+	clisp cracklepop.lisp
+	
+Should also run under SBCL.
---
+++ cracklepop.lisp
@@ -0,0 +1,24 @@
+#!/usr/bin/clisp
+;; Define function to create the list from 1 to 100
+(defun create-range (start end)
+  (progn
+    (nconc 
+     (if (= start end)
+	 (cons start nil)
+       (cons start (create-range (1+ start) end))))))
+
+
+(defun cracklepop (list)
+  (if list
+      (let ((number (pop list)))
+	(progn
+	  (print
+	   (cond ((and (= (mod number 3) 0) (= (mod number 5) 0)) "CracklePop")
+		 ((= (mod number 3) 0) "Crackle")
+		 ((= (mod number 5) 0) "Pop")
+		 (t  number)))
+	  (cracklepop list)))))
+
+
+;; Call the cracklepop function on the generated list
+(cracklepop (create-range 1 100))
